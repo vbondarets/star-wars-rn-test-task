a17b629d6a0beb5826cce58f6794f5558bf7fd03
Volodymyr Bondarets <bondaretsdirect@gmail.com>
2024-08-09 18:26:45 +0300

    feat: Get icon update

--- src/utils/get-icon.util.tsx
+++ src/utils/get-icon.util.tsx
@@ -1,7 +1,15 @@
 import { IconEnum } from '../types/icon.type'
+import User from '../../assets/icons/user-icon-svgrepo-com.svg'
+import Like from '../../assets/icons/like-svgrepo-com.svg'
 
 const getIcon = (type?: IconEnum) => {
   switch (type) {
+    case IconEnum.USER: {
+      return User
+    }
+    case IconEnum.LIKE: {
+      return Like
+    }
     default: {
       return <></>
     }
